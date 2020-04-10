66c29f0197e7af8b21d83adf7ae6d9d303c8f04f
Mitja Podgorelec <mitja.podgorelec@positiva.si>
2020-04-10 09:46:56 +0200

    binder

--- src/android/si/positiva/plugins/background_gps/BackgroundService.java
+++ src/android/si/positiva/plugins/background_gps/BackgroundService.java
@@ -2,6 +2,8 @@ package si.positiva.plugins.background_gps;
 
 import android.app.Service;
 import android.os.Binder;
+import android.os.IBinder;
+import android.content.Intent;
 
 public class BackgroundService extends Service
 {
@@ -18,4 +20,9 @@ public class BackgroundService extends Service
         super.onCreate();
         System.out.println("mitja start background");
     }
+	@Override
+    public IBinder onBind (Intent intent) {
+        return binder;
+    }
+	private final IBinder binder = new BackgroundBinder();
 }
