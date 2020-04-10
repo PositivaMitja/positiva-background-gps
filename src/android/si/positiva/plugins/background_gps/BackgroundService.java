f367c08cb85e3e182022f67d9b0ff16ff14ffb08
Mitja Podgorelec <mitja.podgorelec@positiva.si>
2020-04-10 12:40:54 +0200

    waklock

--- src/android/si/positiva/plugins/background_gps/BackgroundService.java
+++ src/android/si/positiva/plugins/background_gps/BackgroundService.java
@@ -4,6 +4,8 @@ import android.app.Service;
 import android.os.Binder;
 import android.os.IBinder;
 import android.content.Intent;
+import android.os.PowerManager;
+import static android.os.PowerManager.PARTIAL_WAKE_LOCK;
 
 public class BackgroundService extends Service
 {
@@ -19,10 +21,19 @@ public class BackgroundService extends Service
     {
         super.onCreate();
         System.out.println("mitja start background");
+		keepAwake();
     }
 	@Override
     public IBinder onBind (Intent intent) {
         return binder;
     }
 	private final IBinder binder = new BackgroundBinder();
+	@SuppressLint("WakelockTimeout")
+    private void keepAwake()
+    {
+        PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PARTIAL_WAKE_LOCK, "BackgroundService:wakelock");
+        wakeLock.acquire();
+    }
+	private PowerManager.WakeLock wakeLock;
 }
